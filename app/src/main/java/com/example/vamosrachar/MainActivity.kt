83c94b9870c756446bb1f40a31297837ea51669e
kingnaldoo <reinaldosn2016@gmail.com>
2022-10-11 15:54:56 -0300

    feature/add declaration components

--- app/src/main/java/com/example/vamosrachar/MainActivity.kt
+++ app/src/main/java/com/example/vamosrachar/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.vamosrachar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var etPrice: EditText
+    lateinit var etPersons: EditText
+    lateinit var result: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etPrice = findViewById(R.id.price)
+        etPersons = findViewById(R.id.persons)
+        result = findViewById(R.id.result)
+
+        
     }
+
 }
