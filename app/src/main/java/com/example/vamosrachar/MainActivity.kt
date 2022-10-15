ceb122dc14b6b9b93c974fdd96c01056235ee790
kingnaldoo <reinaldosn2016@gmail.com>
2022-10-14 22:32:36 -0300

    add share function

--- app/src/main/java/com/example/vamosrachar/MainActivity.kt
+++ app/src/main/java/com/example/vamosrachar/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.vamosrachar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -9,11 +10,14 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     lateinit var etPrice: EditText
     lateinit var etPersons: EditText
     lateinit var result: TextView
+    lateinit var shareButton: FloatingActionButton
+    lateinit var soundButton:  FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +26,8 @@ class MainActivity : AppCompatActivity() {
         etPrice = findViewById(R.id.price)
         etPersons = findViewById(R.id.persons)
         result = findViewById(R.id.result)
+        shareButton = findViewById(R.id.share)
+        soundButton = findViewById(R.id.sound)
 
         etPrice.addTextChangedListener {
             if(!etPrice.text.isNullOrEmpty() && !etPersons.text.isNullOrEmpty()) {
@@ -39,6 +45,18 @@ class MainActivity : AppCompatActivity() {
                 val calc = String.format("%.2f", priceValue/personsValue)
                 result.text = "R$ $calc"
             }
+        }
+
+        fun share(msg: String) {
+            var intent = Intent()
+            intent.setAction(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_TEXT, msg)
+            intent.setType("text/plain")
+            startActivity(intent)
+        }
+
+        shareButton!!.setOnClickListener{
+            share(result.text.toString())
         }
     }
 
