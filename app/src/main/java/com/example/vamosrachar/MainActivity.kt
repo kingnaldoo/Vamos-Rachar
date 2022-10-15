f46ced9be17d2b8f8b24c584dd9b2df4b38a4f69
kingnaldoo <reinaldosn2016@gmail.com>
2022-10-14 22:01:08 -0300

    make division

--- app/src/main/java/com/example/vamosrachar/MainActivity.kt
+++ app/src/main/java/com/example/vamosrachar/MainActivity.kt
@@ -6,11 +6,14 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
     lateinit var etPrice: EditText
     lateinit var etPersons: EditText
-    lateinit var result: EditText
+    lateinit var result: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +23,23 @@ class MainActivity : AppCompatActivity() {
         etPersons = findViewById(R.id.persons)
         result = findViewById(R.id.result)
 
-        
+        etPrice.addTextChangedListener {
+            if(!etPrice.text.isNullOrEmpty() && !etPersons.text.isNullOrEmpty()) {
+                val priceValue = etPrice.text.toString().toDouble()
+                val personsValue = etPersons.text.toString().toDouble()
+                val calc = String.format("%.2f", priceValue/personsValue)
+                result.text = "R$ $calc"
+            }
+        }
+
+        etPersons.addTextChangedListener {
+            if(!etPrice.text.isNullOrEmpty() && !etPersons.text.isNullOrEmpty()) {
+                val priceValue = etPrice.text.toString().toDouble()
+                val personsValue = etPersons.text.toString().toDouble()
+                val calc = String.format("%.2f", priceValue/personsValue)
+                result.text = "R$ $calc"
+            }
+        }
     }
 
 }
